35e00ba73afd844c4906597693e71001077724bd
carlos reyes <andrespk3000@gmail.com>
2024-04-03 00:45:53 -0500

    index view settingup

--- hosaik/hosaik/urls.py
+++ hosaik/hosaik/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from .views import homeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homeView, name='index'),
     path('accounts/', include('apps.accounts.urls'), name='accounts'),
     path('blog/', include('apps.blog.urls'), name='blog'),
     path('honey/', include('apps.honey.urls'), name='honey'),
